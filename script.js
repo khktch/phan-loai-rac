dbc4161dba33e2733843f68e8152530bb0759736
khktch <hientranspvatly@gmail.com>
2025-11-05 19:35:46 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let model, webcam, labelContainer, maxPredictions;
+
+async function init() {
+  const modelURL = "model.json";
+  const metadataURL = "metadata.json";
+
+  model = await tmImage.load(modelURL, metadataURL);
+  const flip = true;
+  webcam = new tmImage.Webcam(200, 200, flip);
+  await webcam.setup();
+  await webcam.play();
+  window.requestAnimationFrame(loop);
+
+  document.getElementById("webcam-container").appendChild(webcam.canvas);
+  labelContainer = document.getElementById("label-container");
+  for (let i = 0; i < model.getTotalClasses(); i++) {
+    labelContainer.appendChild(document.createElement("div"));
+  }
+}
+
+async function loop() {
+  webcam.update();
+  await predict();
+  window.requestAnimationFrame(loop);
+}
+
+async function predict() {
+  const prediction = await model.predict(webcam.canvas);
+  for (let i = 0; i < prediction.length; i++) {
+    labelContainer.childNodes[i].innerHTML =
+      prediction[i].className + ": " + prediction[i].probability.toFixed(2);
+  }
+}
